88999aec757e257cc972271dfc25288c20e928ad
五木老祖 <wumulaozu@gmail.com>
2019-09-30 11:42:39 +0800

    客户端资源

---
+++ public/build/precache-manifest.eef93e6738aa7849203e83a4cefd5f20.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a768d12a4ed33aab78f7b440028f3d27",
+    "url": "/index.html"
+  },
+  {
+    "revision": "825fc3277d1024542ed3",
+    "url": "/static/css/main.c39f2ad8.chunk.css"
+  },
+  {
+    "revision": "5a7fd14734787863090b",
+    "url": "/static/js/2.e9d00d8b.chunk.js"
+  },
+  {
+    "revision": "825fc3277d1024542ed3",
+    "url": "/static/js/main.f6fe5c93.chunk.js"
+  },
+  {
+    "revision": "703c3901598c8b4cfd4f",
+    "url": "/static/js/runtime~main.c110c5c7.js"
+  },
+  {
+    "revision": "d7e60f9d1433a45ed71817f6d23abeca",
+    "url": "/static/media/icons.d7e60f9d.woff2"
+  }
+]);
