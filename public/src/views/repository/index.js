9654ecf033baeb1bba9f19158d7d4dd6fc7a7fca
五木老祖 <wumulaozu@gmail.com>
2019-11-07 22:33:21 +0800

    1

--- public/src/views/repository/index.js
+++ public/src/views/repository/index.js
@@ -1,13 +1,14 @@
 import React from 'react';
-import {withStyles } from '@material-ui/core/styles';
+import {withStyles} from '@material-ui/core/styles';
 import AddIcon from '@material-ui/icons/Add';
 import DeleteIcon from '@material-ui/icons/Delete';
 import Fingerprint from '@material-ui/icons/Fingerprint';
 import Computer from '@material-ui/icons/Computer';
 import Announcement from '@material-ui/icons/Announcement';
+import LinkIcon from '@material-ui/icons/Link'
 import {
-    Dialog,DialogContent,DialogTitle,DialogContentText,Tooltip,
-    DialogActions,Button,IconButton,Fab,Paper,TableRow,TableHead,TableCell,TableBody,Table
+    Dialog, DialogContent, DialogTitle, DialogContentText, Tooltip,
+    DialogActions, Button, IconButton, Fab, Paper, TableRow, TableHead, TableCell, TableBody, Table
 } from '@material-ui/core';
 import ApiUrl from '../../config/url.js'
 import RepositoryStatus from './repositoryStatus.js'
@@ -35,9 +36,9 @@ class RepositoryTable extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            tableData:[],
-            destroyDialogShow:false,
-            editCategoryShow:false
+            tableData: [],
+            destroyDialogShow: false,
+            editCategoryShow: false
         };
         this.destroyId = 0;
     }
@@ -46,39 +47,44 @@ class RepositoryTable extends React.Component {
         this.getTableData()
     }
 
-    getTableData(){
-        repositories().then(r=>{
-            this.setState({tableData:r})
-        }).catch(()=>{})
+    getTableData() {
+        repositories().then(r => {
+            this.setState({tableData: r})
+        }).catch(() => {
+        })
     }
 
-    destroyDialogOpen(id){
+    destroyDialogOpen(id) {
         this.destroyId = id;
-        this.setState({destroyDialogShow:true})
+        this.setState({destroyDialogShow: true})
     }
 
-    destroyDialogClose () {
-        this.setState({destroyDialogShow:false})
+    destroyDialogClose() {
+        this.setState({destroyDialogShow: false})
     }
-    destroyConfirm () {
+
+    destroyConfirm() {
         // destroy({id:this.destroyId}).then(r=>{
         //     this.setState({destroyDialogShow:false});
         //     this.getTableData()
         // }).catch(()=>{})
     }
-    editDialogShow () {
-        this.setState({editCategoryShow:true})
-    }
-    editDialogClose () {
-        this.setState({editCategoryShow:false})
+
+    editDialogShow() {
+        this.setState({editCategoryShow: true})
     }
 
-    createCategorySuccess(){
-        this.setState({editCategoryShow:false});
+    editDialogClose() {
+        this.setState({editCategoryShow: false})
+    }
+
+    createCategorySuccess() {
+        this.setState({editCategoryShow: false});
         this.getTableData()
     }
+
     render() {
-        const { classes } = this.props;
+        const {classes} = this.props;
         return (
             <div>
                 <Paper className={classes.root}>
@@ -107,7 +113,7 @@ class RepositoryTable extends React.Component {
                                     </TableCell>
                                     <TableCell align="left">{row.Name}</TableCell>
                                     <TableCell align="left">
-                                        <RepositoryStatus status={row.Status} />
+                                        <RepositoryStatus status={row.Status}/>
                                     </TableCell>
                                     <TableCell align="left">
                                         <Tooltip title={
@@ -117,32 +123,43 @@ class RepositoryTable extends React.Component {
                                                 密码：{row.Password}
                                             </div>
                                         } interactive>
-                                            <IconButton  color="primary">
-                                                <Fingerprint />
+                                            <IconButton color="primary">
+                                                <Fingerprint/>
                                             </IconButton>
                                         </Tooltip>
                                     </TableCell>
                                     <TableCell align="left">
-                                        <Tooltip title={row.ConsoleOutput} interactive>
-                                            <IconButton  color="primary">
-                                                <Computer />
+                                        <Tooltip title={
+                                            <div style={{whiteSpace: "pre-wrap"}}>
+                                                {row.ConsoleOutput}
+                                            </div>
+                                        } interactive>
+                                            <IconButton color="primary">
+                                                <Computer/>
                                             </IconButton>
                                         </Tooltip>
                                     </TableCell>
                                     <TableCell align="left">
                                         <Tooltip title={row.Desc} interactive>
-                                            <IconButton  color="primary">
-                                                <Announcement />
+                                            <IconButton color="primary">
+                                                <Announcement/>
                                             </IconButton>
                                         </Tooltip>
                                     </TableCell>
-                                    <TableCell align="left">{row.Url}</TableCell>
+                                    <TableCell align="left">
+                                        <Tooltip title={row.Url} interactive>
+                                            <IconButton color="primary">
+                                                <LinkIcon/>
+                                            </IconButton>
+                                        </Tooltip>
+                                    </TableCell>
                                     <TableCell align="left">{row.WebHookSecret}</TableCell>
                                     <TableCell align="left">{ApiUrl}/web_hook?id={row.ID}</TableCell>
                                     <TableCell align="left">{row.CreatedAt}</TableCell>
                                     <TableCell align="left">
-                                        <IconButton edge="start" color="primary" onClick={this.destroyDialogOpen.bind(this,row.ID)}>
-                                            <DeleteIcon />
+                                        <IconButton edge="start" color="primary"
+                                                    onClick={this.destroyDialogOpen.bind(this, row.ID)}>
+                                            <DeleteIcon/>
                                         </IconButton>
                                     </TableCell>
                                 </TableRow>
@@ -172,7 +189,7 @@ class RepositoryTable extends React.Component {
                     </DialogActions>
                 </Dialog>
                 <Fab color="primary" className={classes.fab} aria-label="add" onClick={this.editDialogShow.bind(this)}>
-                    <AddIcon />
+                    <AddIcon/>
                 </Fab>
                 <EditCategory
                     show={this.state.editCategoryShow}
