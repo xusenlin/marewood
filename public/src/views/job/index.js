4d6690247fa4e3490cac38ecb438f4d3e34a45a8
五木老祖 <wumulaozu@gmail.com>
2020-06-17 19:58:25 +0800

    add Pagination

--- public/src/views/job/index.js
+++ public/src/views/job/index.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import {withStyles} from '@material-ui/core/styles';
 import {
-    Tab, Tabs, Paper, Fab
+  Tab, Tabs, Paper, Fab
 } from '@material-ui/core';
+import Pagination from '@material-ui/lab/Pagination';
 import {jobsFind} from '../../api/job'
 import {categories} from '../../api/category'
 import CategoriesTable from "./categoriesTable"
@@ -10,133 +11,151 @@ import Edit from "./edit"
 import AddIcon from '@material-ui/icons/Add';
 
 
-
 const styles = theme => ({
-    root: {
-        width: '100%',
-        marginTop: theme.spacing(1),
-        overflowX: 'auto',
-    },
-    table: {
-        minWidth: 650,
-    },
-    fab: {
-        position: 'absolute',
-        bottom: theme.spacing(2),
-        right: theme.spacing(2),
-    },
+  root: {
+    width: '100%',
+    marginTop: theme.spacing(1),
+    overflowX: 'auto',
+  },
+  table: {
+    minWidth: 650,
+  },
+  fab: {
+    position: 'absolute',
+    bottom: theme.spacing(2),
+    right: theme.spacing(2),
+  },
+  pagination:{
+    width: '100%',
+    display: "flex",
+    justifyContent: "center",
+    textAlign:"center",
+    padding: theme.spacing(3),
+  }
 });
 
 
 class Jobs extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            category:0,
-            categories:[],
-            jobs:[],
-            editDialog:{
-                show:false,
-                categoryId:0,
-                categoryName:""
-            }
-        };
-        this.timeoutId = null;
-    }
+  constructor(props) {
+    super(props);
+    this.state = {
+      category: 0,
+      categories: [],
+      jobs: [],
+      editDialog: {
+        show: false,
+        categoryId: 0,
+        categoryName: ""
+      }
+    };
+    this.timeoutId = null;
+  }
 
-    componentDidMount() {
+  componentDidMount() {
 
-        categories().then(r=>{
-            if (r.length === 0){
-                alert("请先新建分类");
-                return
-            }
-            this.setState({categories:r},()=>{
-                this.setTabAndJobsByCategoryId(0)
-            });
-        }).catch(()=>{})
+    categories().then(r => {
+      if (r.length === 0) {
+        alert("请先新建分类");
+        return
+      }
+      this.setState({categories: r}, () => {
+        this.setTabAndJobsByCategoryId(0)
+      });
+    }).catch(() => {
+    })
+  }
+
+  tabsChange(event, index) {
+    this.setTabAndJobsByCategoryId(index)
+  }
+
+  setTabAndJobsByCategoryId(index = 0) {
+    let categoryId = this.state.categories[index].ID;
+    if (this.timeoutId) {
+      clearTimeout(this.timeoutId)
     }
-    tabsChange(event, index){
-        this.setTabAndJobsByCategoryId(index)
-    }
-    setTabAndJobsByCategoryId(index = 0){
-        let categoryId = this.state.categories[index].ID;
-        if(this.timeoutId){
-            clearTimeout(this.timeoutId)
+    jobsFind({id: categoryId}).then(r => {
+      this.setState({category: index, jobs: r});
+      for (let i = 0; i < r.length; i++) {
+        if (r[i].Status === 3) {
+          this.timeoutId = setTimeout(() => {
+            this.setTabAndJobsByCategoryId(index)
+          }, 2000);
+          return
         }
-        jobsFind({id:categoryId}).then(r=>{
-            this.setState({category:index,jobs:r});
-            for (let i = 0; i < r.length; i++) {
-                if (r[i].Status === 3) {
-                    this.timeoutId = setTimeout(()=>{
-                        this.setTabAndJobsByCategoryId(index)
-                    },2000);
-                    return
-                }
+      }
+    }).catch(() => {
+    })
+  }
+
+  editDialogShow() {
+    let category = this.state.categories[this.state.category];
+    this.setState({
+      editDialog: {
+        show: true,
+        categoryId: category.ID,
+        categoryName: category.Name
+      }
+    })
+  }
+
+  createSuccess() {
+    this.setTabAndJobsByCategoryId(this.state.category);
+    this.setState({
+      editDialog: {
+        show: false,
+        categoryId: 0,
+        categoryName: ""
+      }
+    });
+  }
+
+  createClose() {
+    this.setState({
+      editDialog: {
+        show: false,
+        categoryId: 0,
+        categoryName: ""
+      }
+    });
+  }
+
+  render() {
+    const {classes} = this.props;
+    return (
+      <div>
+        <Paper className={classes.root}>
+          <Tabs
+            value={this.state.category}
+            indicatorColor="primary"
+            textColor="primary"
+            centered
+            onChange={this.tabsChange.bind(this)}
+          >
+            {
+              this.state.categories.map(category => (
+                <Tab key={category.ID} label={category.Name}/>
+              ))
             }
-        }).catch(()=>{})
-    }
-    editDialogShow() {
-        let category = this.state.categories[this.state.category];
-        this.setState({
-            editDialog: {
-                show: true,
-                categoryId: category.ID,
-                categoryName:category.Name
-            }
-        })
-    }
-    createSuccess() {
-        this.setTabAndJobsByCategoryId(this.state.category);
-        this.setState({
-            editDialog: {
-                show: false,
-                categoryId: 0,
-                categoryName:""
-            }
-        });
-    }
-    createClose() {
-        this.setState({
-            editDialog: {
-                show: false,
-                categoryId: 0,
-                categoryName:""
-            }
-        });
-    }
-    render() {
-        const {classes} = this.props;
-        return (
-            <div>
-                <Paper className={classes.root}>
-                    <Tabs
-                        value={this.state.category}
-                        indicatorColor="primary"
-                        textColor="primary"
-                        centered
-                        onChange={this.tabsChange.bind(this)}
-                    >
-                        {
-                            this.state.categories.map(category=>(
-                                <Tab key={category.ID} label={category.Name} />
-                            ))
-                        }
-                    </Tabs>
-                    <CategoriesTable tableData={this.state.jobs} refresh={this.setTabAndJobsByCategoryId.bind(this,this.state.category)}/>
-                </Paper>
-                <Fab color="primary" className={classes.fab} aria-label="add" onClick={this.editDialogShow.bind(this)}>
-                    <AddIcon/>
-                </Fab>
-                <Edit show={this.state.editDialog.show}
-                      categoryId={this.state.editDialog.categoryId}
-                      categoryName={this.state.editDialog.categoryName}
-                      handleClose={this.createClose.bind(this)}
-                      createSuccess={this.createSuccess.bind(this)}
-                />
-            </div>
-        );
-    }
+          </Tabs>
+          <CategoriesTable tableData={this.state.jobs}
+                           refresh={this.setTabAndJobsByCategoryId.bind(this, this.state.category)}/>
+          <div className={classes.pagination}>
+            <Pagination count={10}  color="primary"  shape="rounded" />
+          </div>
+        </Paper>
+        <Fab color="primary" className={classes.fab} aria-label="add" onClick={this.editDialogShow.bind(this)}>
+          <AddIcon/>
+        </Fab>
+        <Edit show={this.state.editDialog.show}
+              categoryId={this.state.editDialog.categoryId}
+              categoryName={this.state.editDialog.categoryName}
+              handleClose={this.createClose.bind(this)}
+              createSuccess={this.createSuccess.bind(this)}
+        />
+      </div>
+    );
+  }
 
 }
 
