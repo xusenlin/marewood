bfb26b44a18b6946a5f8be7ba3df9964df18f022
五木老祖 <wumulaozu@gmail.com>
2020-07-23 19:48:25 +0800

    打包

---
+++ public/build/precache-manifest.5e6ea8de697bf3f965e7c9a30d05f09f.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2c58f442599af1a684ba3bccfde02bb8",
+    "url": "./index.html"
+  },
+  {
+    "revision": "7bf28f8da5aac076f096",
+    "url": "./static/css/2.e6157a56.chunk.css"
+  },
+  {
+    "revision": "30d9fa77227138894114",
+    "url": "./static/css/main.4e51d40d.chunk.css"
+  },
+  {
+    "revision": "7bf28f8da5aac076f096",
+    "url": "./static/js/2.7d79bd48.chunk.js"
+  },
+  {
+    "revision": "30d9fa77227138894114",
+    "url": "./static/js/main.c816d03e.chunk.js"
+  },
+  {
+    "revision": "bc60ffce3f16c6395a97",
+    "url": "./static/js/runtime~main.11594d06.js"
+  },
+  {
+    "revision": "d7e60f9d1433a45ed71817f6d23abeca",
+    "url": "./static/media/icons.d7e60f9d.woff2"
+  },
+  {
+    "revision": "afcbb5389e2085970577149bce9ed978",
+    "url": "./static/media/logo.afcbb538.svg"
+  }
+]);
