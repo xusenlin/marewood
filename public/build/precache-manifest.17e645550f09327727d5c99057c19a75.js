afa8ef9fcda2758df8d1ba03b3f4acb807a82331
五木老祖 <wumulaozu@gmail.com>
2020-07-24 16:48:48 +0800

    1

---
+++ public/build/precache-manifest.17e645550f09327727d5c99057c19a75.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    revision: "172fa38654ba11e71f2193e825447052",
+    url: "./index.html"
+  },
+  {
+    revision: "7bf28f8da5aac076f096",
+    url: "./static/css/2.e6157a56.chunk.css"
+  },
+  {
+    revision: "776e7650a21dd916ae64",
+    url: "./static/css/main.4e51d40d.chunk.css"
+  },
+  {
+    revision: "7bf28f8da5aac076f096",
+    url: "./static/js/2.7d79bd48.chunk.js"
+  },
+  {
+    revision: "776e7650a21dd916ae64",
+    url: "./static/js/main.5ffd1ed6.chunk.js"
+  },
+  {
+    revision: "bc60ffce3f16c6395a97",
+    url: "./static/js/runtime~main.11594d06.js"
+  },
+  {
+    revision: "d7e60f9d1433a45ed71817f6d23abeca",
+    url: "./static/media/icons.d7e60f9d.woff2"
+  },
+  {
+    revision: "afcbb5389e2085970577149bce9ed978",
+    url: "./static/media/logo.afcbb538.svg"
+  }
+]);
